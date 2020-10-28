20466dbff71a30d29449be90c54cf0a78f623d42
Vu Anh <anhv.ict91@gmail.com>
2020-10-28 17:36:24 +0700

    initial commit

---
+++ script.py
@@ -0,0 +1,7 @@
+from regex_tokenize import tokenize
+
+f = open("data/UNEWS-100K-V1.txt")
+text = 'Còn ngư dân thì còn bám biển, ông Trọng khẳng định. Tp. ... =>'
+tokens = tokenize(text)
+
+print(0)
