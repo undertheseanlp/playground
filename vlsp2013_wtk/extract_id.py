b81d7b1e36806038b937668471f88ca740fba81e
Vu Anh <anhv.ict91@gmail.com>
2020-11-09 14:22:25 +0700

    GH-2: update vlsp2013_wtk

--- vlsp2013_wtk/extract_id.py
+++ vlsp2013_wtk/extract_id.py
@@ -1,5 +1,5 @@
 import re
-lines = open("data/validate_log.txt").read().splitlines()
+lines = open("tmp/validate_log.txt").read().splitlines()
 lines = [line for line in lines if "L3 Format tokenize-error" in line]
 matched = [re.findall("(train|test)-s(?P<id>\d+)", line)[0] for line in lines]
 train = [_[1] for _ in matched if _[0] == "train"]
