c93eabc6c725724b1c258679ebb84807eed3a30f
Vu Anh <anhv.ict91@gmail.com>
2020-11-09 14:18:58 +0700

    GH-2: addvlsp2013_wtk

--- vlsp2013_wtk/extract_id.py
+++ vlsp2013_wtk/extract_id.py
@@ -4,7 +4,7 @@ lines = [line for line in lines if "L3 Format tokenize-error" in line]
 matched = [re.findall("(train|test)-s(?P<id>\d+)", line)[0] for line in lines]
 train = [_[1] for _ in matched if _[0] == "train"]
 test =  [_[1] for _ in matched if _[0] == "test"]
-with open("data/validate_ids.yml", "w") as f:
+with open("../data/validate_ids.yml", "w") as f:
     f.write("train_ignores: |-\n")
     f.write("    " + ",".join(train) + "\n")
     f.write("test_ignores: |-\n")
