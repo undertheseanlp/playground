7a31e02ebc98ba0ac19bf07c2b28d0011f05fe28
Vu Anh <anhv.ict91@gmail.com>
2020-11-11 14:12:58 +0700

    GH-2: update predict function

--- vlsp2013_wtk/predict.py
+++ vlsp2013_wtk/predict.py
@@ -1,15 +1,50 @@
 from pathlib import Path
+
+import pycrfsuite
 import yaml
 from underthesea.file_utils import CACHE_ROOT
 
-
 # underthesea.models.crf_sequence_tagger
+from underthesea.transformer.tagged import TaggedTransformer
+from underthesea.word_tokenize import tokenize
+
+
 class CRFSequenceTagger:
-    def __init__(self):
-        pass
+    def __init__(self, features=None, estimator=None):
+        self.features = features
+        self.estimator = None
 
     def load(self, base_path):
         print(base_path)
+        model_path = str(Path(base_path) / "model.tmp")
+        estimator = pycrfsuite.Tagger()
+        estimator.open(model_path)
+        self.estimator = estimator
+
+    def predict(self, tokens):
+        features = [
+            # word unigram and bigram and trigram
+            "T[-2]", "T[-1]", "T[0]", "T[1]", "T[2]",
+            "T[-2,-1]", "T[-1,0]", "T[0,1]", "T[1,2]",
+            "T[-2,0]", "T[-1,1]", "T[0,2]",
+
+            "T[-2].lower", "T[-1].lower", "T[0].lower", "T[1].lower", "T[2].lower",
+            "T[-2,-1].lower", "T[-1,0].lower", "T[0,1].lower", "T[1,2].lower",
+
+            "T[-1].isdigit", "T[0].isdigit", "T[1].isdigit",
+
+            "T[-2].istitle", "T[-1].istitle", "T[0].istitle", "T[1].istitle", "T[2].istitle",
+            "T[0,1].istitle", "T[0,2].istitle",
+
+            "T[-2].is_in_dict", "T[-1].is_in_dict", "T[0].is_in_dict", "T[1].is_in_dict", "T[2].is_in_dict",
+            "T[-2,-1].is_in_dict", "T[-1,0].is_in_dict", "T[0,1].is_in_dict", "T[1,2].is_in_dict",
+            "T[-2,0].is_in_dict", "T[-1,1].is_in_dict", "T[0,2].is_in_dict",
+        ]
+        transformer = TaggedTransformer(features)
+        tokens = [(token, "X") for token in tokens]
+        x = transformer.transform([tokens])[0]
+        tags = self.estimator.tag(x)
+        return tags
 
 
 def load_model(base_path):
@@ -24,6 +59,22 @@ def load_model(base_path):
     return None
 
 
+def word_tokenize(sentence):
+    tokens = tokenize(sentence)
+    tags = model.predict(tokens)
+    output = []
+    for tag, token in zip(tags, tokens):
+        if tag == "I-W":
+            output[-1] = output[-1] + u" " + token
+        else:
+            output.append(token)
+    if format == "text":
+        output = u" ".join([item.replace(" ", "_") for item in output])
+    return output
+
+
 base_path = Path(CACHE_ROOT) / "models/wtk_crf_2"
 model = load_model(base_path)
-print(model)
+sentence = 'Chàng trai 9X Quảng Trị khởi nghiệp từ nấm sò'
+output = word_tokenize(sentence)
+print(output)
