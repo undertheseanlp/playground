e9a8c0389767f9598a2eb8ea6d8391167db3f5fb
Vu Anh <anhv.ict91@gmail.com>
2020-11-23 10:22:02 +0700

    add revise script

--- vlsp2020_dp/train.py
+++ vlsp2020_dp/train.py
@@ -10,7 +10,7 @@ model_name = 'dp-model'
 MODELS_FOLDER = f'{CACHE_ROOT}/models/vlsp2020_dp'
 model_file = f'{MODELS_FOLDER}/{model_name}'
 
-VLSP2020_DP_FOLDER = f'{DATASETS_FOLDER}/VLSP2020-DP-O'
+VLSP2020_DP_FOLDER = f'{DATASETS_FOLDER}/VLSP2020-DP-R1'
 train_file = f'{VLSP2020_DP_FOLDER}/DP-Package2.18.11.2020.txt'
 train_cmd = f'java -jar {MALT_PARSER}/maltparser-1.9.2.jar -c {model_name} -i {train_file} -m learn'
 print(train_cmd)
