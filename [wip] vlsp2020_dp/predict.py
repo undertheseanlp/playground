afe40f164628f88ca5a0193c8f57cda3e5feb736
Vu Anh <anhv.ict91@gmail.com>
2020-11-23 11:46:14 +0700

    add predict script

---
+++ [wip] vlsp2020_dp/predict.py
@@ -0,0 +1,8 @@
+from underthesea import pos_tag
+
+text = 'Rồi tui rang gạo lấy nước pha sữa bò, bón cho nó từng muỗng.'
+
+text = 'Đó là kết của cuộc vật lộn bền bỉ gần 17 năm của Huỳnh Đỗi.'
+
+output = pos_tag(text)
+print(output)
