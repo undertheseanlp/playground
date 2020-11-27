af03195846cf52327748f2c645deab93691be137
Vu Anh <anhv.ict91@gmail.com>
2020-11-27 17:12:59 +0700

    update

--- wip_vlsp2020_dp/predict_supar.py
+++ wip_vlsp2020_dp/predict_supar.py
@@ -8,5 +8,5 @@ from wip_vlsp2020_dp.export.parsers.parser import Parser
 
 parser = Parser.load('./tmp/dp')
 
-dataset = parser.predict([['Đó', 'là', 'kết', 'của', 'cuộc', 'vật lộn', 'bền bỉ', 'gần', '17', 'năm', 'của', 'Huỳnh', 'Đỗi', '.']], verbose=False)
+dataset = parser.predict([['Đó', 'là', 'kết quả', 'của', 'cuộc', 'vật lộn', 'bền bỉ', 'gần', '17', 'năm', 'của', 'Huỳnh Đỗi', '.']], verbose=False)
 print(dataset.sentences)
