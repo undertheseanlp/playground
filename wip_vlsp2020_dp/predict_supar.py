66a2b301928d77ff0dd637eedd4f66b094e25d91
Vu Anh <anhv.ict91@gmail.com>
2020-11-27 17:13:38 +0700

    update

--- wip_vlsp2020_dp/predict_supar.py
+++ wip_vlsp2020_dp/predict_supar.py
@@ -1,11 +1,5 @@
 from wip_vlsp2020_dp.export.parsers.parser import Parser
 
-# parser = Parser.load('biaffine-dep-en')
-#
-# dataset = parser.predict([['She', 'enjoys', 'playing', 'tennis', '.']], prob=True, verbose=False)
-# print(dataset)
-
-
 parser = Parser.load('./tmp/dp')
 
 dataset = parser.predict([['Đó', 'là', 'kết quả', 'của', 'cuộc', 'vật lộn', 'bền bỉ', 'gần', '17', 'năm', 'của', 'Huỳnh Đỗi', '.']], verbose=False)
