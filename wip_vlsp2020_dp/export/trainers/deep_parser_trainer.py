f8c9d2898d08afa813c3a320602908d0ae0d2808
anhv <anhv.ict91@gmail.com>
2020-11-27 20:51:16 +0700

    update

--- wip_vlsp2020_dp/export/trainers/deep_parser_trainer.py
+++ wip_vlsp2020_dp/export/trainers/deep_parser_trainer.py
@@ -1,7 +1,23 @@
+from os.path import join
+from underthesea.file_utils import CACHE_ROOT
+from wip_vlsp2020_dp.export.models.biaffine_dependency import BiaffineDependencyParser
+
 class DeepParserTrainer:
     def __init__(self, parser, corpus):
         self.parser = parser
         self.corpus = corpus
 
     def train(self, max_epochs=10):
-        pass
+        DATASETS_FOLDER = join(CACHE_ROOT, 'datasets')
+        CORPUS = join(DATASETS_FOLDER, 'VLSP2020-DP-R1')
+        args = {
+            'feat': 'char',
+            'build': True,
+            'train': f'{CORPUS}/train.txt',
+            'test': f'{CORPUS}/test.txt',
+            'dev': f'{CORPUS}/dev.txt',
+            'embed': False
+        }
+
+        parser = BiaffineDependencyParser.build(path='tmp/dp', **args)
+        parser.train(train=args['train'], dev=args['dev'], test=args['test'], epochs=max_epochs)
--- wip_vlsp2020_dp/supar_train.py
+++ wip_vlsp2020_dp/supar_train.py
@@ -1,24 +1,6 @@
-from os.path import join
-from underthesea.file_utils import CACHE_ROOT
-
 from wip_vlsp2020_dp.export.trainers.deep_parser_trainer import DeepParserTrainer
-from wip_vlsp2020_dp.export.models.biaffine_dependency import BiaffineDependencyParser
-
-DATASETS_FOLDER = join(CACHE_ROOT, 'datasets')
-CORPUS = join(DATASETS_FOLDER, 'VLSP2020-DP-R1')
-args = {
-    'feat': 'char',
-    'build': True,
-    'train': f'{CORPUS}/train.txt',
-    'test': f'{CORPUS}/test.txt',
-    'dev': f'{CORPUS}/dev.txt',
-    'embed': False
-}
-
-parser = BiaffineDependencyParser.build(path='tmp/dp', **args)
-parser.train(train=args['train'], dev=args['dev'], test=args['test'], epochs=5)
 
 parser = None
 corpus = None
 trainer = DeepParserTrainer(parser, corpus)
-trainer.train(max_epochs=30)
+trainer.train(max_epochs=5)
