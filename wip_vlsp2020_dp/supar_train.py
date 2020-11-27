0d7c3d7e2ca5193b0c7f1608922626ae8bfb1513
anhv <anhv.ict91@gmail.com>
2020-11-27 22:05:30 +0700

    update

--- wip_vlsp2020_dp/supar_train.py
+++ wip_vlsp2020_dp/supar_train.py
@@ -1,5 +1,5 @@
 from export.data import VLSP2020_DP_R1, Corpus
-from export.models.biaffine_dependency_parser import BiaffineDependencyParserSupar, BiaffineDependencyParser
+from export.models.biaffine_dependency_parser import BiaffineDependencyParser
 from wip_vlsp2020_dp.export.trainers.deep_parser_trainer import DeepParserTrainer
 
 corpus: Corpus = VLSP2020_DP_R1()
@@ -7,4 +7,4 @@ corpus: Corpus = VLSP2020_DP_R1()
 parser = BiaffineDependencyParser()
 trainer = DeepParserTrainer(parser, corpus)
 trainer.train(base_path='tmp/dp',
-              max_epochs=2)
+              max_epochs=1)
