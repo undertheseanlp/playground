733d12130b0a2013e887a202d6703a0f41259bf4
anhv <anhv.ict91@gmail.com>
2021-01-18 13:58:24 +0700

    update

--- plays/dependency_parsing/vlsp2020_dp_sample.py
+++ plays/dependency_parsing/vlsp2020_dp_sample.py
@@ -8,18 +8,29 @@ from underthesea.trainers.dependency_parser_trainer import DependencyParserTrain
 
 corpus = VLSP2020_DP_SAMPLE()
 
+from os.path import join
+
+from underthesea.datasets.vlsp2020_dp import VLSP2020_DP_SAMPLE
+from underthesea.file_utils import MODELS_FOLDER
+from underthesea.models.dependency_parser import DependencyParser
+from underthesea.modules.embeddings import FieldEmbeddings, CharacterEmbeddings
+from underthesea.trainers.dependency_parser_trainer import DependencyParserTrainer
+
+corpus = VLSP2020_DP_SAMPLE()
+
 embeddings = [
     FieldEmbeddings(),
     CharacterEmbeddings()
 ]
-base_path = join(MODELS_FOLDER, 'parsers', 'vi-dp-v1a0')
 parser = DependencyParser(embeddings=embeddings, init_pre_train=True)
 trainer: DependencyParserTrainer = DependencyParserTrainer(parser, corpus)
+base_path = join(MODELS_FOLDER, 'parsers', 'vi-dp-sample')
 trainer.train(
     base_path=base_path,
     max_epochs=3,
     mu=.9  # optimizer parameters
 )
+
 
 parser = DependencyParser.load(base_path)
 sentences = [
