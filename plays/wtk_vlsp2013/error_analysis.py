604206e2e4cb88b2c0af7cc6387a114e124b09fe
anhv <anhv.ict91@gmail.com>
2021-04-25 20:53:36 +0700

    update

---
+++ plays/wtk_vlsp2013/error_analysis.py
@@ -0,0 +1,39 @@
+from os.path import join
+from pathlib import Path
+import yaml
+from underthesea.models.crf_sequence_tagger import CRFSequenceTagger
+from underthesea.word_tokenize import tokenize
+
+
+def load_model(base_path):
+    with open(Path(base_path) / "model.metadata", "r") as f:
+        metadata = yaml.safe_load(f)
+    model_type = metadata["model"]
+    if model_type == "CRFSequenceTagger":
+        model = CRFSequenceTagger()
+        model.load(base_path)
+        return model
+    return None
+
+
+def word_tokenize(tagger, sentence):
+    tokens = tokenize(sentence)
+    tags = tagger.predict(tokens)
+    output = []
+    for tag, token in zip(tags, tokens):
+        if tag == "I-W":
+            output[-1] = output[-1] + u" " + token
+        else:
+            output.append(token)
+    if format == "text":
+        output = u" ".join([item.replace(" ", "_") for item in output])
+    return output
+
+
+if __name__ == '__main__':
+    base_path = join("outputs", "2021-04-25", "_16-49-39", "wtk_custom_crf_2")
+    tagger = load_model(base_path)
+    sentence = 'Chàng trai 9X Quảng Trị khởi nghiệp từ nấm sò'
+    output = word_tokenize(tagger, sentence)
+    print(output)
+
